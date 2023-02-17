c8295ffd73f3a992a224bf30faf28fda21a65e3c
yenle259 <vile0229@gmail.com>
2023-02-17 16:56:23 +0700

    use localStorage

--- src/stores/ToDoStore.js
+++ src/stores/ToDoStore.js
@@ -3,9 +3,8 @@ import { defineStore } from "pinia";
 
 export const useToDoStore = defineStore('todoStore', {
     state: () => ({
-        todo: [ ],
-        name: ":vv",
-        isLoading:false
+        todo: [],
+        isLoading: false
     }),
     getters: {
         completed() {
@@ -17,59 +16,53 @@ export const useToDoStore = defineStore('todoStore', {
             }, 0)
         },
         totalCount(state) {
-            return state.todo.length != 0 ? state.todo.length:0
+            return state.todo.length != 0 ? state.todo.length : 0
         }
     },
     actions: {
-        async getToDo() {
-            this.isLoading = true
-
-            const res = await fetch('http://localhost:3000/todo')
-            const data = await res.json()
-
-            this.todo = data
-            this.isLoading = false
-        },
-        async addToDo(task) {
-            this.todo.push(task)
-
-            const res = await fetch('http://localhost:3000/todo',{
-                method:'POST',
-                body:JSON.stringify(task),
-                headers:{'Content-Type':'application/json'}
-            })
-
-            if(res.error){
-                console.log(res.error);
+        getToDo() {
+            // console.log(localStorage.getItem('todo'))
+            if (localStorage.getItem('todo')) {
+                try {
+                    this.todo = JSON.parse(localStorage.getItem('todo'));
+                } catch (e) {
+                    localStorage.removeItem('todo');
+                }
             }
         },
-        async removeTask(id) {
+        addToDo(task) {
+            this.todo.push(task);
+            this.saveToDo()
+        },
+        saveToDo() {
+            let parsed = JSON.stringify(this.todo);
+            localStorage.setItem('todo', parsed);
+        },
+        removeTask(id) {
             this.todo = this.todo.filter(t => {
                 return t.id !== id
             })
-
-            const res = await fetch('http://localhost:3000/todo/'+id,{
-                method:'DELETE',
-            })
-
-            if(res.error){
-                console.log(res.error);
-            }
+            this.saveToDo();
         },
-        async toggleCompleteStatus(id) {
+        toggleCompleteStatus(id) {
             const task = this.todo.find(t => t.id === id)
-            // console.log(task);
+            console.log(task);
             task.isCompleted = !task.isCompleted
-
-            const res = await fetch('http://localhost:3000/todo/'+id,{
-                method:'PATCH',
-                body:JSON.stringify({isCompleted: task.isCompleted}),
-                headers:{'Content-Type':'application/json'}
-            })
-
-            if(res.error){
-                console.log(res.error);
-            }
+            this.saveToDo();
         }
+        // async toggleCompleteStatus(id) {
+        //     const task = this.todo.find(t => t.id === id)
+        //     task.isCompleted = !task.isCompleted
+
+        //     const res = await fetch('http://localhost:3000/todo/' + id, {
+        //         method: 'PATCH',
+        //         body: JSON.stringify({ isCompleted: task.isCompleted }),
+        //         headers: { 'Content-Type': 'application/json' }
+        //     })
+
+        //     if (res.error) {
+        //         console.log(res.error);
+        //     }
+        // }
     }
 })
